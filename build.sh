74055381cdcecc613f5bbab34c6a98b36d6e5fc8
flowerinthenight <root@flowerinthenight.com>
2024-09-10 14:06:58 +0900

    ci: trigger build

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ echo "Compiling proto files..." && buf generate
 
 echo "Generating OpenAPI docs..."
 protoc -I . --openapiv2_out ./openapiv2 --openapiv2_opt logtostderr=true --openapiv2_opt allow_merge=true \
-       info/v1/*.proto
+       ./info/v1/*.proto
 
 echo "Generating html docs..."
 protoc --doc_out=./generated/ --doc_opt=html,index.html \
-       info/v1/*.proto
+       ./info/v1/*.proto
